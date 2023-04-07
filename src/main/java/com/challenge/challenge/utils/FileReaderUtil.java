a3379eaf8b043016264580bc9deef7952acddf17
João Monteiro <jcsmonteiro1994@gmail.com>
2023-04-07 15:23:57 +0100

    added file reader util class

---
+++ src/main/java/com/challenge/challenge/utils/FileReaderUtil.java
@@ -0,0 +1,35 @@
+package com.challenge.challenge.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class FileReaderUtil {
+
+  public static List<String> readFile(String zonesFilesPath) {
+
+    try (BufferedReader r = new BufferedReader(new FileReader(zonesFilesPath))) {
+      List<String> lines = new ArrayList<>();
+      String s = r.readLine();
+
+      while (s != null) {
+        lines.add(s);
+        s = r.readLine();
+      }
+
+      return lines;
+    } catch (IOException e){
+      e.printStackTrace();
+    }
+
+    return new ArrayList<>();
+  }
+
+  public static String getFilePath(String file){
+    return String.format("src/main/resources/data/%s", file);
+  }
+}
